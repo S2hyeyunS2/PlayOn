dc5fd2a457b7c2aaf19a50d8a6e3aef7202f4115
김혜윤 <hhyeyun22@gmail.com>
2026-01-02 19:13:09 +0900

    [refactor] 길드 게시판 목록 조회 N+1 문제 해결 (#2)

--- src/main/java/com/ll/playon/domain/guild/guildBoard/repository/GuildBoardCommentRepository.java
+++ src/main/java/com/ll/playon/domain/guild/guildBoard/repository/GuildBoardCommentRepository.java
@@ -4,12 +4,21 @@ import com.ll.playon.domain.guild.guildBoard.entity.GuildBoard;
 import com.ll.playon.domain.guild.guildBoard.entity.GuildBoardComment;
 import com.ll.playon.domain.guild.guildMember.entity.GuildMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface GuildBoardCommentRepository extends JpaRepository<GuildBoardComment, Long> {
     List<GuildBoardComment> findByBoardOrderByCreatedAtAsc(GuildBoard board);
-    int countByBoard(GuildBoard board);
     List<GuildBoardComment> comment(String comment);
     void deleteByAuthor(GuildMember author);
+    @Query("""
+    SELECT gbc.board.id, COUNT(gbc.id)
+    FROM GuildBoardComment gbc
+    WHERE gbc.board.id IN :boardIds
+    GROUP BY gbc.board.id
+""")
+    Map<Long, Long> countByBoardIds(@Param("boardIds") List<Long> boardIds);
 }
